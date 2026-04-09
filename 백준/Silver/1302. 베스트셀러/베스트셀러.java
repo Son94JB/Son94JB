1542ea9aed6119504cf4455e63477cc3669b61d1
Son94JB <125803158+Son94JB@users.noreply.github.com>
2026-04-09 12:44:43 +0900

    [Silver IV] Title: 베스트셀러, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -3,6 +3,24 @@ import java.util.*;
 
 public class Main {
 
+    static class Book implements Comparable<Book>{
+        String title;
+        int sold;
+
+        Book(String title, int sold){
+            this.title = title;
+            this.sold = sold;
+        }
+
+        @Override
+        public int compareTo(Book o) {
+            if (this.sold != o.sold) {
+                return o.sold - this.sold;
+            }
+            return this.title.compareTo(o.title);
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         // 오늘 팔린 책의 이름이 입력으로 들어올 때,
         // 가장 많이 팔린 책의 이름을 출력
@@ -11,21 +29,30 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int max = 0;
-        String bestseller = "";
 
         HashMap<String, Integer> booksSold = new HashMap<>();
 
         for (int i = 0; i < n; i++) {
             String book = br.readLine();
             booksSold.put(book, booksSold.getOrDefault(book, 0) + 1);
-            if (booksSold.get(book) > max ||
-                    (booksSold.get(book) == max && book.compareTo(bestseller) < 0)) {
-                max = booksSold.get(book);
-                bestseller = book;
-            }
         }
 
-        System.out.println(bestseller);
+        Book bestseller = booksSold.entrySet().stream()
+                .map(e -> new Book(e.getKey(), e.getValue()))
+                .min(Comparator.naturalOrder())
+                .get();
+//        위 코드를 풀어 쓰면 다음과 같다.
+//
+//        List<Book> bookList = new ArrayList<>();
+//        for (Map.Entry<String, Integer> entry : booksSold.entrySet()) {
+//            bookList.add(new Book(entry.getKey(), entry.getValue()));
+//        }
+//
+//        Collections.sort(bookList);
+//
+//        Book bestseller = bookList.get(0);
+
+        System.out.println(bestseller.title);
 
     }
 }
