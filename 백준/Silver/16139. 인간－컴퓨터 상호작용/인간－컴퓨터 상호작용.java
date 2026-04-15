4eed4310632e30e3ab6e5315a563540b9d6e8485
Son94JB <125803158+Son94JB@users.noreply.github.com>
2026-04-15 11:24:07 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 756 ms, Memory: 114304 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -2,38 +2,39 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static int[] prefix;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        String str = br.readLine();
-        int q = Integer.parseInt(br.readLine());
 
+        String str = br.readLine();
+        int n = str.length();
+
+        // 미리 알파벳 전체에 대한 누적합을 만들어 두기
+        int[][] prefix = new int[n][26];
+
+        // 첫 번째 문자 초기화
+        prefix[0][str.charAt(0) - 'a'] = 1;
+
+        // 2차원 누적합 한 번만 계산 O(N)
+        for (int i = 1; i < n; i++) {
+            for (int c = 0; c < 26; c++) {
+                prefix[i][c] = prefix[i - 1][c];
+            }
+            prefix[i][str.charAt(i) - 'a']++;
+        }
+
+        int q = Integer.parseInt(br.readLine());
+        
         for (int i = 0; i < q; i++) {
             String[] inputs = br.readLine().split(" ");
-            char a = inputs[0].charAt(0);
-
-            // prefix 배열 초기화
-            prefix = new int[str.length()];
-
-            // 누적합 계산
-            prefix[0] = (str.charAt(0) == a) ? 1 : 0;
-
-            for (int j = 1; j < str.length(); j++) {
-                if (str.charAt(j) == a) {
-                    prefix[j] = prefix[j - 1] + 1;
-                } else {
-                    prefix[j] = prefix[j - 1];
-                }
-            }
-
+            int a = inputs[0].charAt(0) - 'a';
             int l = Integer.parseInt(inputs[1]);
             int r = Integer.parseInt(inputs[2]);
 
-            // l이 0이면 그냥 prefix[r], 아니면 prefix[r] - prefix[l-1]
-            int result = (l == 0) ? prefix[r] : prefix[r] - prefix[l - 1];
-            sb.append(result).append("\n");
+            int result = (l == 0) ? prefix[r][a] : prefix[r][a] - prefix[l - 1][a];
+            sb.append(result).append('\n');
         }
+
         System.out.print(sb);
     }
 }
